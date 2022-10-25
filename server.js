ae8e390a98e6f2a175ecd37e3070ef238240ddb0
Thomas McCarthy <tommccarthyguitar@gmail.com>
2022-10-25 14:09:14 -0500

    add port env var

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port`)
 })
